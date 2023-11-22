528b3d82fef56d3934cd99a9c47d70c12e242d6a
Jewel Rana <juyelrana7752@gmail.com>
2023-11-22 18:57:31 +0600

    30 % ok app

---
+++ src/route/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../page/Home";
+
+  const route = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main />,
+        children:[
+            {
+                path:'/',
+                element:<Home />
+            }
+        ]
+
+    }
+  ])
+
+  export default route;
